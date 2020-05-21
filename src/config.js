b907c37af1a755a318c94d92ec5bf23be46f4b3b
Gabriel Alvarez <galvarez.chile@gmail.com>
2020-05-21 19:05:45 -0400

    Se incluye variable de entorno para heroku.

--- src/config.js
+++ src/config.js
@@ -6,10 +6,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // cadena de conexión a base de datos!
 let urlConnectionDB;
-const passwordCloud = 'dDJyP1gX1KAFlFmT'; // clave de cada cluster en mongodb cloud
 if ( process.env.NODE_ENV === 'dev' ){
     urlConnectionDB = 'mongodb://localhost:27017/db-cafe';
 }else{
-    urlConnectionDB = `mongodb+srv://root:${passwordCloud}@herculescluster-0jd1d.gcp.mongodb.net/cafe-api`;
+    urlConnectionDB = process.env.MONGO_URI;
 }
 process.env.URI_DATA_BASE = urlConnectionDB;
